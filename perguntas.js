85d5c4dfe169e1524fadf776df53292480f86ea7
Istalisson <149580590+stalisson@users.noreply.github.com>
2024-09-23 15:52:15 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'ciencia',
+    ' Qual é o processo pelo qual as plantas convertem luz solar em energia?',
+    'O processo pelo qual as plantas convertem luz solar em energia é a fotossíntese.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é o maior país do mundo em extensão territorial?',
+    ' O maior país do mundo em extensão territorial é a Rússia.'
+)
+
+criaCartao(
+    'Futebol',
+    'Quem é o jogador com mais gols na história da Copa do Mundo?',
+    ' O jogador com mais gols na história da Copa do Mundo é Miroslav Klose, da Alemanha, com 16 gols.'
+)
+
+criaCartao(
+    'Futebol',
+    'Qual clube europeu tem mais títulos da Liga dos Campeões da UEFA?',
+    'O clube europeu com mais títulos da Liga dos Campeões da UEFA é o Real Madrid, com 14 títulos.'
+)
